3648c681b3f846c0259d62bd3959b81bd7e54ea9
RogerAylagas <roger.aylagas.torres@protonmail.com>
2021-08-10 19:49:18 +0200

    Unfeasable data partitioning. TODO: Save all samples ina file

--- preprocessing/dataset2windows.py
+++ preprocessing/dataset2windows.py
@@ -3,18 +3,22 @@ from posix import listdir
 from utils import Session
 
 
-def session2windows(path, file, d, w, o, temporalDimension = False):
-    sess = Session(file, path)
-    D = d*sess.SRATE
-    W = w*sess.SRATE
+def session2windows(in_dir, out_dir, file, d, w, o, temp_dim = False):
+    try:
+        sess = Session(file, in_dir)
+    except:
+        sys.exit("ERROR: Corrupted file")
+    D = int(d*sess.SRATE)
+    W = int(w*sess.SRATE)
     O = int(W*o)
     n_trials = sess.get_num_trials()
-    for t in range(1,n_trials+1):
-        trial_data = sess.cut_eeg(t)
-        if(not temporalDimension):
+    for t in range(0,n_trials):
+        trial_data = sess.cut_eeg(t, sess.SRATE)
+        if(not temp_dim):
             #TODO
-            sess.bin_trial(trial_data, bin_length)
-            raise NotImplementedError("Functionality not available :(")
+            file = os.path.splitext(file)[0]
+            sess.bin_trial(trial_data, t, os.path.join(out_dir, file), sess.SRATE, D, W, O, temp_dim)
+            #raise NotImplementedError("Functionality not available :(")
             
         else:
             #TODO
@@ -22,25 +26,34 @@ def session2windows(path, file, d, w, o, temporalDimension = False):
 
 
 
-def dataset2windows(path, temp_dir, d, w, o):
-    files = os.listdir(path)
+def dataset2windows(in_dir, out_dir, temp_dir, d, w, o):
+    files = os.listdir(in_dir)
+    files.sort()
+    '''
     for f in files:
-        session2windows(path, f, d, w, o, temp_dir)
+        session2windows(in_dir, out_dir, f, d, w, o, temp_dir)
+    '''
+    session2windows(in_dir, out_dir, files[0], d, w, o, temp_dir)
+    
 
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         sys.exit("Usage : python session2samples.py input_dir temp_dim\n\t \
         - input_dir: Directory where sessions are located\n\t \
         - temp_dim: split data in temporal windows (1) or return it as an image (0)")
     try:
-        temp_dim = int(sys.argv[2])
+        temp_dim = int(sys.argv[3])
         if(temp_dim!=0 and temp_dim!=1):
             raise ValueError("temp_dim must be either 1 or 0")
     except:
         sys.exit()
-    d = 500
-    w = 40
-    o = 0
-    dataset2windows(sys.argv[1], temp_dim, d, w, o)
+    d = 500*1e-3
+    w = 40*1e-3
+    o = 0*1e-3
+    if(not os.path.exists(sys.argv[1])):
+        sys.exit("Input folder does not exist :(")
+    if(not os.path.exists(sys.argv[2])):
+        os.mkdir(sys.argv[2])
+    dataset2windows(sys.argv[1], sys.argv[2], temp_dim, d, w, o)
--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 from scipy.io import loadmat
 import numpy as np
 import os
+import h5py
 
 class Session:
     def __init__(self, file, path):
@@ -17,13 +18,19 @@ class Session:
         self.time = bci_data[1]                  # Vector of the trial time (in ms) relative to target presentation
         self.positionx = bci_data[2]             # X position cursor during feedback
         self.positiony = bci_data[3]             # Y position of cursor during feedback
-        self.SRATE = bci_data[4]                 # Sampling rate of EEG recording
-        self.trialData = bci_data[5]             # Data structure describing trial level metrics
+        self.SRATE = int(bci_data[4])                 # Sampling rate of EEG recording
+        self.trialData = bci_data[5][0]             # Data structure describing trial level metrics
         self.metadata = bci_data[6]              # Participant and session level demographic information
         self.chaninfo = bci_data[7]              # Information about individual EEG channels
 
     def get_num_trials(self):
-        return len(self.data)
+        return len(self.data[0])
+
+    def get_trial_length(self, trial_n):
+        return self.trialData[trial_n][4]
+
+    def get_target_num(self, trial_n):
+        return self.trialData[trial_n][3]
 
     def get_trial_data(self, trial_n):
         """
@@ -32,21 +39,21 @@ class Session:
             - trial_n (int): trial number to return information for
         """
         
-        taskNumber = self.trialData[0][trial_n][0]          # Identification number for task type
-        runNumber = self.trialData[0][trial_n][1]           # The run to which a trial belongs
-        trialNumber = self.trialData[0][trial_n][2]         # The trial number of a given session
-        targetNumber = self.trialData[0][trial_n][3]        # Identification number for target presented (1=R, 2=L, 3=U, 4=D)
-        trialLength = self.trialData[0][trial_n][4]         # The length of the feedback control period in s
-        targetHitNumber = self.trialData[0][trial_n][5]     # Identification number for target selected by the BCI control
-        resultInd = self.trialData[0][trial_n][6]           # Time index for the end of the feedback control portion of the trial
-        result = self.trialData[0][trial_n][7]              # Outcome of the trial: success or failure
-        forcedResult = self.trialData[0][trial_n][8]        # Outcome of the trial with forced target selection for timeout trials: success or failure
-        artefact = self.trialData[0][trial_n][9]            # Does the trial contain an artefact?
+        taskNumber = self.trialData[trial_n][0]          # Identification number for task type
+        runNumber = self.trialData[trial_n][1]           # The run to which a trial belongs
+        trialNumber = self.trialData[trial_n][2]         # The trial number of a given session
+        targetNumber = self.trialData[trial_n][3]        # Identification number for target presented (1=R, 2=L, 3=U, 4=D)
+        trialLength = self.trialData[trial_n][4]         # The length of the feedback control period in s
+        targetHitNumber = self.trialData[trial_n][5]     # Identification number for target selected by the BCI control
+        resultInd = self.trialData[trial_n][6]           # Time index for the end of the feedback control portion of the trial
+        result = self.trialData[trial_n][7]              # Outcome of the trial: success or failure
+        forcedResult = self.trialData[trial_n][8]        # Outcome of the trial with forced target selection for timeout trials: success or failure
+        artefact = self.trialData[trial_n][9]            # Does the trial contain an artefact?
 
         return taskNumber, runNumber, trialNumber, targetNumber, trialLength, targetHitNumber, resultInd, result, \
                forcedResult, artefact
 
-    def cut_eeg(self, trial_n, pre=None, post=None):
+    def cut_eeg(self, trial_n, sr, start_t=None, end_t=None):
         """
         Removes all data before/after pre/post values for a given trial.
         Parameters:
@@ -56,28 +63,22 @@ class Session:
         Returns:
             - trial_cut (DataFrame): EEG data (channels x time)
         """
-        _, _, _, _, triallength, _, _, _, _, _ = self.get_trial_data(trial_n)
+        triallength = self.get_trial_data(trial_n)
         # Set our post_trial value to be the full trial length by default
-        if post == None:
-            post_trial = int((triallength[0][0]*1000))+2000
-        else:
-            post_trial = post
+        if end_t == None:
+            end_t = int(-1 * sr)
+
         # Set our pre_trial value to be the start of the feedback-control period (2000ms after 0)
-        if pre == None:
-            pre_trial = -2000
-        else:
-            pre_trial = pre
+        if start_t == None:
+            start_t = 4*sr+1
 
         trial_data = self.data[0][trial_n]
-        trial_time = self.time[0][trial_n]
-        dataframe = pd.DataFrame(trial_data, columns=trial_time[0])
-        range_t = list(range(-pre_trial,post_trial+1))
-        trial_cut = dataframe[range_t]
+        trial_cut = trial_data[:, start_t:end_t]
 
         return trial_cut
 
 
-    def bin_trial(self, trial_cut, binlength=500, delay=40, overlap=0):
+    def bin_trial(self, trial_cut, num_trial, file_name, sr, binlength=500, delay=40, overlap=0, temp_dim=0, save=True):
         """
         Splits a trial into multiple overlapping bins of EEG data.
         
@@ -88,15 +89,28 @@ class Session:
         Returns:
             - bins (list) - A list containing EEG arrays (channels x time)
         """
-        n_bins = ((trial_cut.shape[1]-binlength)//delay)+1         # Calculate the number of bins we can get from this trial
-        bins = []
-        start_t = 0                                            # Set a counter for which timepoint to start each bin on
-        for i in range(1,n_bins+1):
-            bin = trial_cut.iloc[:,start_t:start_t+binlength]
-            bins.append(bin.to_numpy())
-            start_t += delay                                   # Add our delay value to the counter
+        if(temp_dim==0):
+            print(trial_cut.shape[1])
+            n_bins = (trial_cut.shape[1]-(binlength-delay))//delay         # Calculate the number of bins we can get from this trial
+            bins = []                                            # Set a counter for which timepoint to start each bin on
+            for i in range(0,n_bins):
+                bin = trial_cut[:, i*delay : i*delay+binlength]
+                label = self.get_target_num(num_trial)
+                if save:
+                    hf = h5py.File(file_name+'_trial_{}_window_{}'.format(num_trial+1,i+1)+'.h5', 'w')
+                    hf.create_dataset('data', data=bin)
+                    hf.create_dataset('label', data=label)
+                    hf.close()
+                    #raise NotImplementedError("Function not available yet :(")
+                else:
+                    bins.append(bin)
 
-        return bins
+            if not save:
+                return bins
+        else:
+            raise NotImplementedError("Function not available yet :(")    # Add our delay value to the counter
+
+        
 
 
     def get_x_y(self, pre=None, post=None):
@@ -111,7 +125,7 @@ class Session:
         """
         inputs = []         # Create a blank list to store our input data arrays
         labels = []         # Create another list to store the label arrays
-        for trial in range(0,len(self.data[0])):
+        for trial in range(0,len(self.data[0])-1):
             if trial%50==0:
                 print(f"Trial: {trial}/{len(self.data[0])}")
             _, _, _, targetnumber, _, _, _, _, _, _ = self.get_trial_data(trial)

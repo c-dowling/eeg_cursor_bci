Correct loss calculation

main.py
@@ -27,7 +27,7 @@ def main():
     print("Model Number Parameters = ", count_parameters(model))
     early_stopping = EarlyStopping(patience=10)
     optimizer = optim.Adam(model.parameters(), params['lr'])
-    criterion = torch.nn.CrossEntropyLoss(reduction='mean')
+    criterion = torch.nn.CrossEntropyLoss(reduction='none')
     
     train(model, {'Train': data_loaders['train'], 'Valid': data_loaders['validation']}, optimizer, criterion, params, callback=early_stopping)
 

utils.py
@@ -133,7 +133,7 @@ def train(model, dataloaders, optimizer, criterion, params, callback=None):
                     loss = criterion(outputs, labels)
 
                     if phase == 'Train':
-                        loss.backward()
+                        loss.mean().backward()
                         optimizer.step()
                 
                 probabilities_task = F.softmax(outputs, dim=1)